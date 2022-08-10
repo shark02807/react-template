cc06b7280cf899e20bdb5c8040f0c40baa9bd58c
Aleksandr Belik <shark02807@gmail.com>
2022-08-11 00:19:53 +0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/extensions': [0, { allow: 'never' }]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  }
+};
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# react-template
+# React-Template
+
+## Stacks
+
+- React
+- Typescript
+- PostCSS
+- Tailwindcss
+
+## Tools for development stage
+
+- ESlint
+- Prettier
+- Husky pre-commit (with lint-staged)
+
+## Available Scripts
+
+Clone the repository:
+
+```
+  $ git clone https://github.com/shark02807/react-template.git
+```
+
+In the project directory, you can install dependencies:
+
+```
+  $ npm install
+```
+
+And run the app in the development mode:
+
+```
+  $ npm start
+```
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+The page will reload if you make edits.
